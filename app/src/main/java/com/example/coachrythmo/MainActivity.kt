0b8a88b09e31f80eea8d9964d260aa4d51ad459e
diarracse <mamediarrac13@gmail.com>
2026-03-27 22:56:50 -0400

    ajout formulaire page routine

--- app/src/main/java/com/example/coachrythmo/MainActivity.kt
+++ app/src/main/java/com/example/coachrythmo/MainActivity.kt
@@ -12,10 +12,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.coachrythmo.navigation.Screen
+import com.example.coachrythmo.presentation.list.AddRoutineScreen
 import com.example.coachrythmo.presentation.list.ListRoutinesScreen
 import com.example.coachrythmo.presentation.list.ListRoutinesViewsModel
 import com.example.coachrythmo.ui.theme.CoachRythmoTheme
-import com.example.coachrythmo.navigation.Screen
+import androidx.lifecycle.ViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +25,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoachRythmoTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val navController = rememberNavController()
+                    val sharedViewModel = viewModel<ListRoutinesViewsModel>()
                     NavHost(
                         navController = navController,
                         startDestination = Screen.RoutinesListScreen.route,
                         modifier = Modifier.padding(innerPadding)
                     ) {
                         composable(route = Screen.RoutinesListScreen.route) {
-                            val stories = viewModel<ListRoutinesViewsModel>()
-                            ListRoutinesScreen(navController, stories)
+                            ListRoutinesScreen(navController, sharedViewModel)
+                        }
+                        composable(route = Screen.AddRoutineScreen.route) {
+                            AddRoutineScreen(navController, sharedViewModel)
                         }
                     }
                 }
--- app/src/main/java/com/example/coachrythmo/navigation/Screens.kt
+++ app/src/main/java/com/example/coachrythmo/navigation/Screens.kt
@@ -2,4 +2,5 @@ package com.example.coachrythmo.navigation;
 
 sealed class Screen(val route: String) {
     data object RoutinesListScreen : Screen(route = "routines_list_screen")
+    data object AddRoutineScreen : Screen(route = "add_routine_screen")
 }
