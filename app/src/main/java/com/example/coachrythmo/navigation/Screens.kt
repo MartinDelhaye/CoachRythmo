93f6b60aa0aa86f9bf42b2bf9a5ab398d7d31fd1
diarracse <mamediarrac13@gmail.com>
2026-03-27 23:12:49 -0400

    merge: fusion dev dans feature/ajout-routine

--- app/src/main/java/com/example/coachrythmo/navigation/Screens.kt
+++ app/src/main/java/com/example/coachrythmo/navigation/Screens.kt
@@ -3,10 +3,7 @@ package com.example.coachrythmo.navigation
 sealed class Screen(val route: String) {
     data object HomeScreen : Screen(route = "home")
     data object RoutinesListScreen : Screen(route = "routines_list_screen")
-<<<<<<< HEAD
     data object AddRoutineScreen : Screen(route = "add_routine_screen")
-=======
     data object SuiviScreen : Screen("suivi")
     data object CompteScreen : Screen("compte")
->>>>>>> 3e4fad36a7169813c7d46ee05c6e387674a74330
 }
