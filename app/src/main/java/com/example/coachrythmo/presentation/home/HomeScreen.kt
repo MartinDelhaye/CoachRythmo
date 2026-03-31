82d00818e7f1d17cbca7cf06af38d6b0693347c3
MartinDelhaye <delhayemar1@gmail.com>
2026-03-30 20:16:57 -0400

    Intégration des prochaines séances sur l'accueil :
    - Composant UpcomingSessions
    - Add du défilement vertical sur la page d'accueil

--- app/src/main/java/com/example/coachrythmo/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/coachrythmo/presentation/home/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.example.coachrythmo.presentation.home
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.coachrythmo.presentation.components.AppScreen
 import com.example.coachrythmo.presentation.components.TodaySession
+import com.example.coachrythmo.presentation.components.UpcomingSessions
 import com.example.coachrythmo.presentation.components.WeekCalendar
 import com.example.coachrythmo.presentation.getRoutines
 
@@ -21,11 +25,15 @@ fun HomeScreen(navController: NavController) {
     ) {
 
         Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
 
             WeekCalendar()
             TodaySession(todayRoutine)
+            UpcomingSessions(routines.drop(1))
         }
     }
 }
