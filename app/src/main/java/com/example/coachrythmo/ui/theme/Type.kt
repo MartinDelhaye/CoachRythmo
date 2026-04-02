7539cd11082a5718b1ee26626c501f03724413ea
diarracse <mamediarrac13@gmail.com>
2026-04-02 16:04:19 -0400

    feat: récupération assets et config depuis dev/ajoutRoutine

--- app/src/main/java/com/example/coachrythmo/ui/theme/Type.kt
+++ app/src/main/java/com/example/coachrythmo/ui/theme/Type.kt
@@ -2,38 +2,33 @@ package com.example.coachrythmo.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.coachrythmo.R
 
-val Orbitron = FontFamily(
-    Font(R.font.orbitron_variable_font_wght, FontWeight.Bold)
-)
-
-val Roboto = FontFamily(
-    Font(R.font.roboto_regular, FontWeight.Normal),
-    Font(R.font.roboto_bold, FontWeight.Bold)
-)
-
-// Typography Material3
+// Set of Material typography styles to start with
 val Typography = Typography(
-    titleLarge = TextStyle(
-        fontFamily = Orbitron,
-        fontSize = 30.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = Orbitron,
-        fontSize = 18.sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = Roboto,
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    headlineMedium = TextStyle(
-        fontFamily = Orbitron,
-        fontSize = 17.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
     )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
 )
